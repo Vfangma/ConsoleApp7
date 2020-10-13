510ef6c1d45683d84e97fdc9c862bf63567e96bf
GitHubTest01197 <v-wage@microsoft.com>
2020-10-12 19:50:15 -0700

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
